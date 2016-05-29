cfef9bdc65932536d78e3f838416dbcf3aaa2d7e
PabloEzequiel <javaen.blogspot.com>
2016-05-29 00:25:46 -0300

    ChessLab02 es la cáscara de la grafica Parte III: Agregamos la SeekBar para marcar el progreso Cambiamos la SeekBar para que tenga un icono propio http://javaen.blogspot.com.ar/2016/05/android-chess-y-layout.html http://javaen.blogspot.com.ar/2016/04/android-chess-y-layout-parte-ii.html http://javaen.blogspot.com.ar/2016/04/android-chess-y-layout-parte-iii.html

--- ChessLab02/app/src/androidTest/java/com/probarnocuestanada/crossfitchess/chesslab02/ApplicationTest.java
+++ ChessLab02/app/src/androidTest/java/com/probarnocuestanada/crossfitchess/chesslab02/ApplicationTest.java
@@ -5,6 +5,13 @@ import android.test.ApplicationTestCase;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ *
+ * Created by Pablo Ezequiel on 16/5/16.
+ * <p>
+ * ProbarNoCuestaNada.com
+ * <p>
+ * https://github.com/PabloEzequiel
+ * https://javaen.blogspot.com
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
