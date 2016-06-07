08a1f6b07f3a39299864aea4499372e553aa8f16
PabloEzequiel <javaen.blogspot.com>
2016-06-06 23:41:12 -0300

    ChessLab03 - Agregando el  Menu Sigue al Post http://javaen.blogspot.com.ar/2016/04/chess-svg-vectores-y-drawables-parte-ii.html

--- ChessLab03/app/src/main/java/com/probarnocuestanada/chess/chesslab03/MainActivity.java
+++ ChessLab03/app/src/main/java/com/probarnocuestanada/chess/chesslab03/MainActivity.java
@@ -80,17 +80,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_mate1) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_mate2) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_mate3) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_mate4) {
 
-        } else if (id == R.id.nav_share) {
+      //  } else if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_send) {
+      //  } else if (id == R.id.nav_send) {
 
         }
 
